d4d3f2b1779a4cf0569ad25941d052f6292ac6f3
Esteban <teten08@hotmail.com>
2024-03-30 18:41:58 -0300

    Con la seachbar funcionando

--- dev-challenge-template-master/challenge-dev/src/components/searchbar.jsx
+++ dev-challenge-template-master/challenge-dev/src/components/searchbar.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Searchbar() {
+    const [dataCharacter, setDataCharacter] = useState(null);
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        if (!input) return; 
+        fetch('https://rickandmortyapi.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query: `
+                    query {
+                        characters(filter: { name: "${input}" }) {
+                            results {
+                                id
+                                name
+                                image
+                            }
+                        }
+                    }
+                `
+            })
+        })
+        .then(response => response.json())
+        .then(data => setDataCharacter(data.data.characters.results[0])) 
+        .catch(error => console.error('Error fetching data:', error));
+    }, [input]); 
+
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+    };
+
     return (
         <div>
-            <input type="text" placeholder="Search" />
+            <input 
+                type="text" 
+                placeholder="Search" 
+                value={input} 
+                onChange={handleInputChange} 
+            />
+            {dataCharacter && (
+                <div>
+                    <p>name: {dataCharacter.name}</p>
+                    <img src={dataCharacter.image} alt={dataCharacter.name} />
+                </div>
+            )}
         </div>
     );
 }
